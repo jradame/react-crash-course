3c72c0e0d9f1cac5339f016381eb4fa606ad06e3
justin <jradame@gmail.com>
2025-06-19 12:01:39 -0500

    react-crash-course

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function Home() {
-  return <h1>Home</h1>
+  const [users, setUsers] = useState([]);
+
+  async function main() {
+    const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      main();
+    }, 500); 
+  }, []);
+const pixels = "3px";
+
+  return (
+    <div style={{ border: `${pixels} solid black` }}>
+      <div>Id</div>
+      <div>Name</div>
+      <div>Username</div>
+      <div>Email</div>
+    </div>
+
+
+
+
+    // <div>
+    //   {/* {users.length > 0 ? 
+    //     <h1>{users[0]?.name}</h1>
+    //     : <h1>Loading...</h1>
+    //   } */}
+    // </div>
+  );
 }
 
-export default Home 
+export default Home;
+
+
